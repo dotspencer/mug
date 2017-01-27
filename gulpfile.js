f18696368a0f2342ae07b56b2d3f3304c2dd9362
Spencer Smith <ssmith@egi.utah.edu>
2017-01-27 09:48:39 -0700

    Added gulp-htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-  inline = require('gulp-inline-source');
+  inline = require('gulp-inline-source'),
+  htmlmin = require('gulp-htmlmin');
 
 gulp.task('default', function(){
 
   return gulp.src('./source/index.html')
     .pipe(inline())
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./public'));
 });
